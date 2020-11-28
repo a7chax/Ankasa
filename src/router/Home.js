5e9d34d265fa76f4fba2d87e2e80f598099a0aa0
apirahman <apirahman55@gmail.com>
2020-11-28 18:19:51 +0700

    Add view explore and setup bottom navigation
    Signed-off-by: apirahman <apirahman55@gmail.com>

---
+++ src/router/Home.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Explore, Profile} from '../screen';
+
+import Icons from 'react-native-vector-icons/Ionicons';
+import {View} from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+function Home() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Booking"
+      tabBarOptions={{
+        activeTintColor: '#2395FF',
+        tabStyle: {
+          marginBottom: 10,
+        },
+        style: {
+          height: 70,
+          paddingHorizontal: 28,
+        },
+      }}>
+      <Tab.Screen
+        name="Booking"
+        component={Explore}
+        options={{
+          tabBarLabel: 'My Booking',
+          tabBarIcon: ({color}) => (
+            <Icons name="calendar-outline" color={color} size={28} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Explore"
+        component={Explore}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: () => (
+            <View
+              style={{
+                backgroundColor: '#2395FF',
+                height: 55,
+                width: 55,
+                alignSelf: 'center',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 55,
+                marginTop: 22,
+              }}>
+              <Icons name="compass" color="#fff" size={28} />
+            </View>
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Profile"
+        component={Explore}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color}) => (
+            <Icons name="person-circle-outline" color={color} size={28} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default Home;
--- src/screen/index.js
+++ src/screen/index.js
@@ -1,9 +1,6 @@
-import Login from './auth/Login'
-import Register from './auth/Register'
-import Welcome from './auth/Welcome'
+import Login from './auth/Login';
+import Register from './auth/Register';
+import Welcome from './auth/Welcome';
+import Explore from './home/HomeExplore';
 
-export {
-	Login,
-	Register,
-	Welcome
-}
+export {Login, Register, Welcome, Explore};
