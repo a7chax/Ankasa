50d90a05b16fd5b39f6d02bc156960f1ee2a2b5c
apirahman <apirahman55@gmail.com>
2020-12-01 14:01:02 +0700

    redux destination

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import Auth from './Auth';
 import Profiles from './Profiles';
+import Destination from './Destination';
 
 const reducers = combineReducers({
   Auth,
   Profiles,
+  Destination,
 });
 
 export default reducers;
