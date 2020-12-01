cef96f20a757caddef093facc9101eb7e9d0e597
hnifmaghfur <hnifmaghfur@gmail.com>
2020-12-01 16:44:14 +0700

    page Detail Booking Redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import Auth from './Auth';
 import Profiles from './Profiles';
 import Destination from './Destination';
+import DetailBooking from './DetailBooking';
 
 const reducers = combineReducers({
   Auth,
   Profiles,
   Destination,
+  DetailBooking,
 });
 
 export default reducers;
